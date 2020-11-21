051a4399e3b20a5c0ba37d820af77a055ea32291
jose luis <joseluisblascoortiz@hotmail.com>
2020-11-21 18:20:29 +0100

    ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,71 +4,71 @@ import { withAuth } from "../lib/AuthProvider";			//	<-- UPDATE HERE
 import {Button} from 'react-bootstrap';
 
 
-
-
-
-// class Navbar extends Component {
-//   render(){
-//   return (
-//     <div>
-//       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-//   <Navbar.Brand href="#home">Movie Dick</Navbar.Brand>
-//   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//   <Navbar.Collapse id="responsive-navbar-nav">
-//     <Nav className="mr-auto">
-//       <Nav.Link href="/login">login</Nav.Link>
-//       <Nav.Link href="/signup">sign up</Nav.Link>
-//       <NavDropdown title="Categorías" id="collasible-nav-dropdown">
-//         <NavDropdown.Item href="#action/3.1">more populars</NavDropdown.Item>
-//         <NavDropdown.Item href="#action/3.2">more rated</NavDropdown.Item>
-//         <NavDropdown.Item href="#action/3.3">random</NavDropdown.Item>
-//         <NavDropdown.Divider />
-//       </NavDropdown>
-//     </Nav>
-//     <Nav>
-//       <Nav.Link href="#deets">More deets</Nav.Link>
-//     </Nav>
-//   </Navbar.Collapse>
-// </Navbar>
-//     </div>
-//   );
-//   };
-// };
-
 class Navbar extends Component {
   render() {
 
     const { user, logout, isLoggedin } = this.props;	//	<-- UPDATE HERE
     return (
+      <header class="header">
+  <a href="" class="logo">CSS Nav</a>
+  <input class="menu-btn" type="checkbox" id="menu-btn" />
+  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+  {
+    isLoggedin ?
+    (<>
+  <ul class="menu">
+    
+  
+  
+    <li><a href="#work">Our Work</a></li>
+    <li><a href="#about">About</a></li>
+    <li><a href="#careers">Careers</a></li>
+    <li><a href="#contact" onClick={logout}>Contact</a></li>
+  
+  </ul>
+  </>)
+    : 
+    (<>
+    <ul class="menu">
+    <li><a href="#work">Our Work</a></li>
+    <li><a href="#about">About</a></li>
+    <li><a href="#careers">Careers</a></li>
+    <li><a href="#contact" onClick={logout}>Contact</a></li>
+    
+    </ul>
+    </>)
+    }
+
+</header>
       
-      <nav className="navbar">
-            <Link to={'/'} id='home-btn'>
-          <img className="logonav" src="logos/cola.png" width="40" height="40"></img>
-        </Link>
-        {
-          isLoggedin ?
-          (<>
-            <p className="navbar-user">username: {user.username}</p>	
-            {/* <button className="navbar-button" onClick={logout}>Logout</button> */}
-            <Button variant="warning" onClick={logout}>Logout</Button>
-            <Link to="/movies"> All Movies</Link>
-            <Link to="/random"> Random</Link>
-            <Link to="/private"> My profile</Link>
-            <Link to="/popular"> popular</Link>
-          </>) 
-         : 
-          (<>
-            <Link to="/login">
-              <button className="navbar-button">Login</button>
-            </Link>
-            <br />
-            <Link to="/signup">
-              <button className="navbar-button">Sign Up</button>
-            </Link>
+      // <nav className="navbar">
+      //       <Link to={'/'} id='home-btn'>
+      //     <img className="logonav" src="logos/cola.png" width="40" height="40"></img>
+      //   </Link>
+      //   {
+      //     isLoggedin ?
+      //     (<>
+      //       <p className="navbar-user">username: {user.username}</p>	
+      //       {/* <button className="navbar-button" onClick={logout}>Logout</button> */}
+      //       <Button variant="warning" onClick={logout}>Logout</Button>
+      //       <Link to="/movies"> All Movies</Link>
+      //       <Link to="/random"> Random</Link>
+      //       <Link to="/private"> My profile</Link>
+      //       <Link to="/popular"> popular</Link>
+      //     </>) 
+      //    : 
+      //     (<>
+      //       <Link to="/login">
+      //         <button className="navbar-button">Login</button>
+      //       </Link>
+      //       <br />
+      //       <Link to="/signup">
+      //         <button className="navbar-button">Sign Up</button>
+      //       </Link>
       
-          </>)
-        }
-      </nav>
+      //     </>)
+      //   }
+      // </nav>
     );
   }
 }
