30940932aef3d979eccdc71b170a750302d55cf2
GitSkynet <carlos.curtido@gmail.com>
2020-11-22 17:23:53 +0100

    Styling web V4

--- src/components/MostPopular.js
+++ src/components/MostPopular.js
@@ -28,58 +28,30 @@ class MostPopular extends Component {
 
     render() {
         return (
-        <div className="container">
-            <div className="title-most">
+        <div className="container2">
                 <h1>MOST POPULAR PAGE</h1>
-            </div>
             {this.state.movies.map((eachMovie) => {
-            return (
-                    <div className="cellphone-container">    
-                        <div className="movie">       
-                            <div className="menu2"><i className="material-icons"></i></div>
-                                <div className="movie-img" style={{backgroundImage: `url(${eachMovie.poster})`}}></div>
-                                    <div className="text-movie-cont">
-                                        <div className="mr-grid">
-                                            <div className="col1">
-                                                <h1>{eachMovie.movie_title}</h1>
-                                                <ul className="movie-gen">
-                                                    <li>{eachMovie.content_rating} /</li>
-                                                    <li>{eachMovie.duration} min /</li>
-                                                    <li>{eachMovie.genres}</li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <div className="mr-grid summary-row">
-                                            <div className="col2">
-                                            <h5>SUMMARY</h5>
-                                        </div>
-                                        <div className="col2">
-                                            <ul className="movie-likes">
-                                                <li><i className="material-icons">&#xE813;</i>{eachMovie.movie_facebook_likes}</li>
-                                                <li><i className="material-icons">&#xE813;</i>3</li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div className="mr-grid">
-                                        <div className="col1">
-                                            <p className="movie-description">A group of elderly people are giving interviews about having lived in a climate of crop blight and constant dust reminiscent of The Great 
-                                            Depression of the 1930's. The first one seen is an elderly woman stating her father was a farmer, but did not start out that way. </p>
-                                    </div>
+                return (
+                    <div className="movie_card" id="bright" style={{backgroundImage: `url(${eachMovie.poster})`, backgroundSize: `100%`, backgroundPosition: `center`, backgroundRepeat: `no-repeat`}}>
+                            <div className="info_section">
+                                <div className="movie_header">
+                                    <img className="locandina" src={eachMovie.poster} />
+                                    <h4>{eachMovie.movie_title}</h4>
+                                    <h4>{eachMovie.title_year}, {eachMovie.director_name}</h4>
+                                    <span className="minutes">{eachMovie.duration} min</span>
+                                    <p className="type">{eachMovie.genres}</p>
                                 </div>
-                                <div className="mr-grid actors-row">
-                                    <div className="col1">
-                                        <p className="movie-actors">{eachMovie.actor_1_name}, {eachMovie.actor_2_name}, {eachMovie.actor_3_name}</p>
-                                    </div>
+                                <div className="movie_desc">
+                                    <p className="text">{eachMovie.description}</p>
                                 </div>
-                                <div className="mr-grid action-row">
-                                    <div className="col2"><div className="watch-btn"><h3><i className="material-icons">&#xE037;</i>MORE DETAILS</h3></div>
-                                </div>
-                                <div className="col6 action-btn"><i className="material-icons">&#xE161;</i></div>
-                                <div className="col6 action-btn"><i className="material-icons">&#xE866;</i></div>
-                                <div className="col6 action-btn"><i className="material-icons">&#xE80D;</i></div>
-                                    </div>
+                                <div className="movie_social">
+                                    <ul>
+                                        <li><a href={`/upload/${eachMovie._id}`} className="material-icons">Update movie</a></li>
+                                        <li><a href={`/details/${eachMovie._id}`} className="material-icons">More Details</a></li>
+                                    </ul>
                                 </div>
                             </div>
+                            <div className="blur_back bright_back"></div>
                         </div>
                     );
                 })}
--- src/components/UpdateMovie.js
+++ src/components/UpdateMovie.js
@@ -8,7 +8,12 @@ class UpdateMovie extends Component {
     poster: "",
     genres: "",
     director_name: "",
-    // movie: {}
+    content_rating: "",
+    country: "",
+    language: "",
+    movie_imdb_link: "",
+    title_year:  "",
+    imdb_score:  ""
   };
 
   getDetailsMovie = async () => {
@@ -17,7 +22,16 @@ class UpdateMovie extends Component {
     description: res.description,
     poster: res.poster,
     genres: res.genres,
-    director_name: res.director_name
+    director_name: res.director_name,
+    content_rating: res.content_rating,
+    country: res.country,
+    language: res.language,
+    movie_imdb_link: res.movie_imdb_link,
+    actor_1_name: res.actor_1_name,
+    actor_2_name: res.actor_2_name,
+    actor_3_name: res.actor_3_name,
+    title_year: res.title_year,
+    imdb_score: res.imdb_score
   })
 }
 
@@ -35,15 +49,11 @@ componentDidMount = () => {
     let poster = e.target.files[0]
     // creamos un nuevo objeto FormData
     const uploadData = new FormData();
-
     // poster (este nombre tiene que ser igual que en el modelo, ya que usaremos req.body como argumento del método .create() cuando creemos una nueva movie en la ruta POST '/api/movies/create')
     uploadData.append("poster", e.target.files[0]);
 
     try {
       const res = await service.handleUpload(uploadData);
-
-      
-
       this.setState({ poster: res.secure_url });
     } catch (error) {
       console.log("Error while uploading the file: ", error);
@@ -61,7 +71,16 @@ componentDidMount = () => {
         description: "",
         poster: "",
         genres: "",
-        director_name: ""
+        director_name: "",
+        content_rating: "",
+        country: "",
+        language: "",
+        movie_imdb_link: "",
+        actor_1_name: "",
+        actor_2_name: "",
+        actor_3_name: "",
+        title_year: "",
+        imdb_score: ""
       });
 
       // this.props.getMovies()
@@ -75,7 +94,7 @@ componentDidMount = () => {
     return (
       <div>
         <h2>Update the Movie</h2>
-        <form onSubmit={(e) => this.handleSubmit(e)}>
+        <form onSubmit={(e) => this.handleSubmit(e)} className="edit-form">
           <label htmlFor="">Name</label>
           <input type="text" name="movie_title" value={this.state.movie_title} placeholder={this.state.movie_title} onChange={(e) => this.handleChange(e)}/>
           
@@ -88,7 +107,7 @@ componentDidMount = () => {
             onChange={(e) => this.handleChange(e)}
           />
 
-          <label htmlFor="">Director Name</label>
+          <label htmlFor="">Director Name: </label>
           <input
             type="text"
             name="director_name"
@@ -98,12 +117,93 @@ componentDidMount = () => {
             
           />
 
-          <label htmlFor="">Description</label>
+          <label htmlFor="">Description: </label>
           <input
             type="text"
             name="description"
             value={this.state.description}
             textarea={this.state.description}
+            onChange={(e) => this.handleChange(e)}
+          />
+
+          <label htmlFor="">Rating: </label>
+          <input
+            type="text"
+            name="content_rating"
+            value={this.state.content_rating}
+            placeholder={this.content_rating}
+            onChange={(e) => this.handleChange(e)}
+          />
+
+          <label htmlFor="">Country: </label>
+          <input
+            type="text"
+            name="country"
+            value={this.state.country}
+            placeholder={this.country}
+            onChange={(e) => this.handleChange(e)}
+          />
+
+          <label htmlFor="">Language: </label>
+          <input
+            type="text"
+            name="language"
+            value={this.state.language}
+            placeholder={this.language}
+            onChange={(e) => this.handleChange(e)}
+          />
+
+          <label htmlFor="">IMDB Link:</label>
+          <input
+            type="text"
+            name="movie_imdb_link"
+            value={this.state.movie_imdb_link}
+            placeholder={this.movie_imdb_link}
+            onChange={(e) => this.handleChange(e)}
+          />
+          
+          <label htmlFor="">Actor 1 name:</label>
+          <input
+            type="text"
+            name="actor_1_name"
+            value={this.state.actor_1_name}
+            placeholder={this.actor_1_name}
+            onChange={(e) => this.handleChange(e)}
+          />
+
+          <label htmlFor="">Actor 2 name:</label>
+          <input
+            type="text"
+            name="actor_2_name"
+            value={this.state.actor_2_name}
+            placeholder={this.actor_2_name}
+            onChange={(e) => this.handleChange(e)}
+          />
+
+          <label htmlFor="">Actor 3 name:</label>
+          <input
+            type="text"
+            name="actor_3_name"
+            value={this.state.actor_3_name}
+            placeholder={this.actor_3_name}
+            onChange={(e) => this.handleChange(e)}
+          />
+
+          <label htmlFor="">IMDB Score:</label>
+          <input
+            type="text"
+            name="imdb_score"
+            value={this.state.imdb_score}
+            placeholder={this.imdb_score}
+            onChange={(e) => this.handleChange(e)}
+          />
+
+          <label htmlFor="">Year:</label>
+          <input
+            type="text"
+            name="title_year"
+            value={this.state.title_year}
+            placeholder={this.title_year}
             onChange={(e) => this.handleChange(e)}
           />
 
