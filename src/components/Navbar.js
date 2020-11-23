5e876000c0e317587135e390a39b0ce0d7deae86
GitSkynet <carlos.curtido@gmail.com>
2020-11-23 15:09:00 +0100

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,31 +35,6 @@ class Navbar extends Component {
 export default withAuth(Navbar);
 
 
-{/* <nav className="navbar">
-            <Link to="/" id='home-btn'>
-          <img className="logonav" src="logos/cola.png" width="40" height="40" alt="phoooto"></img>
-        </Link>
-        {
-          isLoggedin ?
-          (<>
-            <p className="navbar-user">username: {user.username}</p>	
-            <button className="navbar-button" onClick={logout}>Logout</button>
-<Button variant="warning" onClick={logout}>Logout</Button>
-          </>) 
-         :
-(<>
-  <Link to="/login">
-    <button className="navbar-button">Login</button>
-  </Link>
-  <br />
-  <Link to="/signup">
-    <button className="navbar-button">Sign Up</button>
-  </Link>
-
-</>)
-        }
-      </nav > */}
-
 
 
 
