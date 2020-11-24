f2245c82a12e3e9dc161508aa27384032c0b5f10
GitSkynet <carlos.curtido@gmail.com>
2020-11-24 10:52:01 +0100

    Paginacion V4

--- src/components/Paginacion.js
+++ src/components/Paginacion.js
@@ -4,8 +4,8 @@ const Paginacion = props => {
     console.log('PROPS PAGINACION', props)
     return(
         <div className="button-page">
-            {/* <button onClick={props.paginaAnterior} type="button" classname="btn">Anterior &larr;</button>
-            <button onClick={props.paginaSiguiente} type="button" classname="btn">Siguiente &rarr;</button> */}
+            <button onClick={props.paginaAnterior} type="button" classname="btn">Anterior &larr;</button>
+            <button onClick={props.paginaSiguiente} type="button" classname="btn">Siguiente &rarr;</button>
         </div>
     )
 }
